e2bafdb877452cdcadaa40b90342903a00bfea7c
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-09-02 19:56:09 -0500

    added jquery

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+
+    $('.gallery-item').hover( function() {
+        $(this).find('.img-title').stop(true, true).fadeIn(300);
+    }, function() {
+        $(this).find('.img-title').stop(true, true).fadeOut(100);
+    });
+});
