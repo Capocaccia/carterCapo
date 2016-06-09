e737f2cef787144366ab4823cbf430ce60e9c6db
Carter Capocaccia <carter.capocaccia@gmail.com>
2016-06-09 18:50:15 -0500

    Adds js to build sections from given array and provides backgrounds to each section

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,25 @@
-let $header = $('.header');
-let $section = $('.section');
+let sections = ['Contact', 'About', 'Projects'];
+let stylingClasses = ['section-first', 'section-second', 'section-third'];
+let backgrounds = ['src/images/car.jpeg', 'src/images/cliffs.jpeg', 'src/images/wave.jpeg'];
+let contentBlocks = $('.content');
+console.log(contentBlocks);
 
-$('.header--h1').on('click', function(){
-    window.location = '/'
-});
+let $header = $('.header');
+$('.home').css('display', 'flex');
+
+for(var i = 0; i < sections.length; i++) {
+    var section = sections[i];
+    var sectionClass = 'section ';
+    var classes = sectionClass.concat(section);
+    $('.header--h1').after('<div class=\''+ classes + ' ' + stylingClasses[i] + '\'><p class="title">' + sections[i] + '</p></div>');
+}
+
+for(var i = 0; i < backgrounds.length; i++){
+    console.log(backgrounds[i]);
+    $( contentBlocks[i + 1] ).css('background-image', 'url(' + backgrounds[i] + ')');
+}
+
+let $section = $('.section');
 
 $section.click(function(){
     let sectionTitle = $(this).children('p').html();
@@ -11,12 +27,8 @@ $section.click(function(){
     contentChange(location);
 });
 
-
-
-
-
 function contentChange(location){
-    $('.' + location).removeClass('hidden');
-    $header.siblings().not(document.getElementsByClassName(location)).addClass('hidden');
+    $('.' + location).removeClass('hidden').css('display', 'flex');
+    $header.siblings().not(document.getElementsByClassName(location)).css('display', 'none');
 };
 
