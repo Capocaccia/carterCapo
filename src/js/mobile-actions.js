7f27e70e87b9cd7f7ff2b11ea90a24ba02552dd4
Carter Capocaccia <carter.capocaccia@gmail.com>
2016-06-29 15:43:37 -0500

    Adds click handler for mobile nav

--- src/js/mobile-actions.js
+++ src/js/mobile-actions.js
@@ -2,3 +2,10 @@ $('.navicon').on('click', function(){
     $(this).parent().siblings('.header').stop().slideToggle();
     $('.navicon').toggleClass('active');
 });
+
+$('.section').on('click', function(){
+    if($('.navicon').hasClass('active')){
+        $('.header').slideUp();
+        $('.navicon').removeClass('active');
+    }
+});
