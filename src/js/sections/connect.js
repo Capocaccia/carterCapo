0e6212c815ff1fecf659b733bb5982874cd4619e
Carter Capocaccia <carter.capocaccia@gmail.com>
2017-12-06 19:31:23 -0600

    adds js files for about page and connect page. Adds base markup to return from map in connect page

---
+++ src/js/sections/connect.js
@@ -0,0 +1,27 @@
+var config = {
+    apiKey: 'AIzaSyDt7EQc_GVKqPHWGtw_nT6osO63hOB4cIs',
+    databaseURL: 'https://cartercapo-a6615.firebaseio.com/',
+    storageBucket: 'cartercapo-a6615'
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().once('value').then(function(snapshot) {
+	return snapshot.child('contact').val();
+}).then((result) => {
+	const buildData = result.page;
+    const contactItems = [result.contactItems];
+	const display = `
+	<div class="${buildData.contentClass}" style="background-image: url('${buildData.background}'); display: flex;">
+		<h2>${buildData.title}</h2>
+		<p class="tagline">${buildData.tagline}</p>
+		<div class="main"></div>
+		<div class="navicon"></div>
+        <div class="contact">
+            ${contactItems.map(item => `<div class="contactItem"><p class="title">${item.title}</p><a class="project--item__link" href="mailto:carter.capocaccia@gmail.com"><img src="${item.icon}" alt="${item.email}"></a></div>`)}
+        </div>
+	</div>
+    `;
+    document.querySelector('.mount').innerHTML = display;
+});
+
