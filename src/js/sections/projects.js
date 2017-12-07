f1a85efe3dcb06e608c8af3c2da8e1ada291cb19
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2017-12-07 11:12:15 -0600

    adds js to render projects on page

--- src/js/sections/projects.js
+++ src/js/sections/projects.js
@@ -1,10 +1,39 @@
-//builds projects
-for(var project in projects){
-    if (!projects.hasOwnProperty(project)) continue;
-    var obj = projects[project];
-    $('<a class="project--item__link"><div class="project--item"><h3 class="project--item__title"></h3><p class="project--item__description"></p><div class="background"></div></div></a>').appendTo('.project');
-    $('.project--item__title:last').html(obj.title);
-    $('.project--item__description:last').html(obj.description);
-    $('.project--item__link:last').attr('href', obj.link).attr('target', '_blank');
-    $('.project--item:last').children('.background').css('background-image', 'url(' + obj.image+ ')');
-}
+var config = {
+    apiKey: 'AIzaSyDt7EQc_GVKqPHWGtw_nT6osO63hOB4cIs',
+    databaseURL: 'https://cartercapo-a6615.firebaseio.com/',
+    storageBucket: 'cartercapo-a6615'
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().once('value').then(function(snapshot) {
+	return snapshot.child('projects').val();
+}).then((result) => {
+	const buildData = result.page;
+    const projectItems = result.projectItems;
+	const display = `
+	<div class="${buildData.contentClass}" style="background-image: url('${buildData.background}'); display: flex;">
+		<h2>${buildData.title}</h2>
+		<p class="tagline">${buildData.tagline}</p>
+		<div class="main"></div>
+		<div class="navicon"></div>
+		<div class="project">
+		  ${projectItems.map(item => `
+		  	<a class="project--item__link" href="${item.link}" target="_blank">
+	        	<div class="project--item">
+					<h3 class="project--item__title">
+						${item.title}
+					</h3>
+					<p class="project--item__description">
+						${item.description}
+					</p>
+					<div class="background" style="background-image: url(${item.image});">
+					</div>
+				</div>
+			</a>`)}
+		  </div>
+	</div>
+    `;
+    document.querySelector('.mount').innerHTML = display;
+});
+
