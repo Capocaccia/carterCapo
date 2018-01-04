bf85bdf3ea7273975978f042fd8a91eda5b8dbe2
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2018-01-04 13:22:21 -0600

    removes console.log

--- src/js/mobile-actions.js
+++ src/js/mobile-actions.js
@@ -1,6 +1,4 @@
 var navicon = document.querySelector('.navicon');
 navicon.addEventListener('click', () => {
-	console.log(this)
 	document.querySelector('.header').classList.toggle('open');
-	
 });
