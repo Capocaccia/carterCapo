6917728d4e037d6d8fdfb42a58ec8b070a425bc3
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2018-01-09 13:49:38 -0600

    adds functionality to write new projects to firebase

---
+++ src/js/sections/admin.js
@@ -0,0 +1,28 @@
+import {renderHeader} from '../header.js';
+
+const getProjectData = () => {
+	let description = document.querySelector('.add-field-description').value;
+	let image = document.querySelector('.add-field-image').value;
+	let url = document.querySelector('.add-field-url').value;
+	let title = document.querySelector('.add-field-title').value;
+
+	writeNewProject(description, image, url, title);
+}
+
+document.querySelector('.add-project-submit').addEventListener('click', getProjectData)
+
+function writeNewProject(desc, image, linkPath, titleText) {
+  // A post entry.
+  var projectData = {
+	description: desc,
+	image: image,
+	link: linkPath,
+	title: titleText,
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('projects/projectItems/').push().key;
+var updates = {};
+  updates['projects/projectItems/' + newPostKey] = projectData;
+  return firebase.database().ref().update(updates);
+}
