8411b41020620c134a8bc9846233d13d68f853a4
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-02-04 09:25:12 -0600

    adds stylsheets and routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <header class="header">
+      <router-link to="/" class="section Carter Capocaccia section-home">
+        <p class="title">Carter Capocaccia</p>
+      </router-link>
+      <router-link to="/about" class="section About section-second">
+        <p class="title">About</p>
+      </router-link>
+      <router-link to="/projects" class="section Projects section-third">
+        <p class="title">Projects</p>
+      </router-link>
+      <router-link to="/connect" class="section Connect section-first">
+        <p class="title">Connect</p>
+      </router-link>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -12,5 +26,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import 'scss/modules/_utility.scss';
+  @import 'scss/modules/_header.scss';
 
 </style>
