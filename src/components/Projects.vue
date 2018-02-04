7a370f3acdda11ae52355857494cddc6c169db72
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-02-04 09:25:48 -0600

    adds markup for project items

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,7 +4,22 @@
         <p class="tagline">{{pageData.tagline}}</p>
         <div class="main"></div>
         <div class="navicon"></div>
+        <div class="project" v-for="item in projectItems">
+            <a class="project--item__link" v-bind:href=item.link target="_blank">
+                <div class="project--item">
+                    <h3 class="project--item__title">
+                        {{item.title}}
+                    </h3>
+                    <p class="project--item__description">
+                        {{item.description}}
+                    </p>
+                    <div class="background" v-bind:style="{ backgroundImage: 'url(' + item.image + ')' }">
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -15,7 +30,8 @@
         name: 'Projects',
         data () {
             return {
-                pageData: []
+                pageData: [],
+                projectItems: []
             }
         },
         mounted(){
@@ -23,12 +39,17 @@
                 return snapshot.child('projects').val();
             }).then((result) => {
                 this.pageData = result.page;
-        })
+                this.projectItems = result.projectItems;
+            })
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
+    @import '../scss/modules/_utility.scss';
+    @import '../scss/modules/_header.scss';
+    @import '../scss/modules/_mobile-nav.scss';
+    @import '../scss/modules/_page-content.scss';
+    @import '../scss/modules/_projects.scss';
 </style>
