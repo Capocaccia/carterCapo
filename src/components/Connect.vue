fba5a9863091ef7660647698b506d70c9b618338
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-02-04 08:59:37 -0600

    imports stylesheets

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,34 +1,49 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
+    <div v-bind:class=pageData.contentClass v-bind:style="{ backgroundImage: 'url(' + pageData.background + ')' }">
+        <h2>{{pageData.title}}</h2>
+        <p class="tagline">{{pageData.tagline}}</p>
+        <div class="main"></div>
+        <div class="navicon"></div>
+        <div class="contact">
+            <div class="contactItem" v-for="item in contactItems">
+                <p class="title">
+                    {{item.title}}
+                </p>
+                <a class="project--item__link" v-bind:href=item.link>
+                    <img v-bind:src=item.icon v-bind:alt=item.email>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
+    import db from '../firebaseConfig'
+
     export default {
-        name: 'HelloWorld',
+        name: 'Connect',
         data () {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                pageData: [],
+                contactItems: []
             }
+        },
+        mounted(){
+            db.database().ref().once('value').then(function(snapshot){
+                return snapshot.child('contact').val();
+            }).then((result) => {
+                this.pageData = result.page;
+                this.contactItems = result.contactItems;
+            })
         }
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    a {
-        color: #42b983;
-    }
+<style lang="scss" scoped>
+    @import '../scss/modules/_utility.scss';
+    @import '../scss/modules/_header.scss';
+    @import '../scss/modules/_mobile-nav.scss';
+    @import '../scss/modules/_page-content.scss';
+    @import '../scss/modules/_contact.scss';
 </style>
