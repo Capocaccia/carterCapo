d5c435c2c1d9aababef543a11aa053c357fa40c8
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-02-04 09:00:00 -0600

    adds sass config and firebase config

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,25 +9,17 @@
 
 <script>
 
-import firebase from 'firebase'
+import db from '../firebaseConfig'
 
-var config = {
-  apiKey: 'AIzaSyDt7EQc_GVKqPHWGtw_nT6osO63hOB4cIs',
-  databaseURL: 'https://cartercapo-a6615.firebaseio.com/',
-  storageBucket: 'cartercapo-a6615'
-}
-
-
-let app = firebase.initializeApp(config);
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data () {
     return {
       pageData: []
     }
   },
   mounted(){
-    app.database().ref().once('value').then(function(snapshot){
+    db.database().ref().once('value').then(function(snapshot){
       return snapshot.child('home').val();
     }).then((result) => {
       this.pageData = result.page;
@@ -37,19 +29,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+
 </style>
