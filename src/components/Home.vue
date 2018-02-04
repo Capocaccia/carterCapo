2a0e7d6c29dfd5c17d9b7fbc5497cec21a359a6b
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-02-03 18:47:16 -0600

    firebase config working and homepage rendering correctly

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div v-bind:class=pageData.contentClass v-bind:style="{ backgroundImage: 'url(' + pageData.background + ')' }">
+    <h2>{{pageData.title}}</h2>
+    <p class="tagline">{{pageData.tagline}}</p>
+    <div class="main"></div>
+    <div class="navicon"></div>
   </div>
 </template>
 
 <script>
+
+import firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyDt7EQc_GVKqPHWGtw_nT6osO63hOB4cIs',
+  databaseURL: 'https://cartercapo-a6615.firebaseio.com/',
+  storageBucket: 'cartercapo-a6615'
+}
+
+
+let app = firebase.initializeApp(config);
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      pageData: []
     }
+  },
+  mounted(){
+    app.database().ref().once('value').then(function(snapshot){
+      return snapshot.child('home').val();
+    }).then((result) => {
+      this.pageData = result.page;
+  })
   }
 }
 </script>
