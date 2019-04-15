9c920c3d3d7f6b11214f85f55bb04a6701e5335d
Carter Capocaccia <ccapocaccia@uaminc.com>
2019-04-15 15:44:50 -0500

    Updates

---
+++ precache-manifest.3639f8144689650bdd5142ef3ecf0aad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03a5062b9b7cf908dabe",
+    "url": "/static/js/main.e759280e.chunk.js"
+  },
+  {
+    "revision": "82293648aff8e6c68cc8",
+    "url": "/static/js/2.86442170.chunk.js"
+  },
+  {
+    "revision": "03a5062b9b7cf908dabe",
+    "url": "/static/css/main.35a4cf0d.chunk.css"
+  },
+  {
+    "revision": "93286d9b460262d1d4f9a2843bbc74be",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Capocaccia/carterCapo/precache-manifest.70e52e86e45d5df96432eb4373dbbb37.js"
+  "/precache-manifest.3639f8144689650bdd5142ef3ecf0aad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Capocaccia/carterCapo/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
