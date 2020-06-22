ebe430d831ff71a6213601f86b1d8bce2c4cd479
Carter Capocaccia <carter.capocaccia@hilton.com>
2020-06-22 17:15:45 -0500

    re-writes connect page as a functional component with hooks

--- src/views/connect.js
+++ src/views/connect.js
@@ -1,50 +1,49 @@
-import db from '../firebaseConfig'
-import React, {Component} from 'react'
+import db from "../firebaseConfig";
+import React, { useEffect, useState } from "react";
 import toggleMobile from "../mixins/toggleMobile";
 
-class Connect extends Component {
-    constructor() {
-        super()
-        this.state = {
-            pageData: null,
-            contactItems: []
-        }
-    }
-    componentDidMount() {
-        db.database().ref().once('value').then(function(snapshot){
-            return snapshot.child('contact').val();
-        }).then((result) => {
-            this.setState({
-                pageData: result.page,
-                contactItems: result.contactItems
-            });
-        })
-    }
-    render() {
-        let items = this.state.contactItems.map((item, idx) => {
-            return  <div key={idx} className='contactItem'>
-                <p className="title">
-                    {item.title}
-                </p>
-                <a className="project--item__link" href={item.link}>
-                    <img src={item.icon} alt={item.email}></img>
-                </a>
-            </div>
-        })
+function Connect() {
+  const [pageData, setPageData] = useState([]);
+  const [contactItems, setContactItems] = useState([]);
 
-        return (
-            <div className={this.state.pageData ? this.state.pageData.contentClass : ''}
-                 style={{backgroundImage: `url(${this.state.pageData ? this.state.pageData.background : ''})`}}>
-                <h2>{this.state.pageData ? this.state.pageData.title : ''}</h2>
-                <p className="tagline">{this.state.pageData ? this.state.pageData.title : ''}</p>
-                <div className="main"></div>
-                <div className="navicon" onClick={() => toggleMobile()}></div>
-                <div className="contact">
-                    {items}
-                </div>
-            </div>
-        )
-    }
+  useEffect(() => {
+    db.database()
+      .ref()
+      .once("value")
+      .then(function(snapshot) {
+        return snapshot.child("contact").val();
+      })
+      .then(result => {
+        setPageData(result.page);
+        setContactItems(result.contactItems);
+      });
+  }, [contactItems]);
+
+  let items = contactItems.map((item, idx) => {
+    return (
+      <div key={idx} className="contactItem">
+        <p className="title">{item.title}</p>
+        <a className="project--item__link" href={item.link}>
+          <img src={item.icon} alt={item.email}></img>
+        </a>
+      </div>
+    );
+  });
+
+  return (
+    <div
+      className={pageData ? pageData.contentClass : ""}
+      style={{
+        backgroundImage: `url(${pageData ? pageData.background : ""})`
+      }}
+    >
+      <h2>{pageData ? pageData.title : ""}</h2>
+      <p className="tagline">{pageData ? pageData.title : ""}</p>
+      <div className="main"></div>
+      <div className="navicon" onClick={() => toggleMobile()}></div>
+      <div className="contact">{items}</div>
+    </div>
+  );
 }
 
-export default Connect
+export default Connect;
