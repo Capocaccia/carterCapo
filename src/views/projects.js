fac5732d1c543a837396926321647ef26da3ccd4
Carter Capocaccia <carter.capocaccia@hilton.com>
2020-06-22 17:22:19 -0500

    re-writes projects page as a functional component with hooks

--- src/views/projects.js
+++ src/views/projects.js
@@ -1,41 +1,40 @@
-import db from '../firebaseConfig'
-import React, { Component } from 'react';
-import Projectlist from '../components/projectsList'
-import toggleMobile from '../mixins/toggleMobile'
+import db from "../firebaseConfig";
+import React, { useEffect, useState } from "react";
+import Projectlist from "../components/projectsList";
+import toggleMobile from "../mixins/toggleMobile";
 
-class Projects extends Component {
-    constructor() {
-        super()
-        this.state = {
-            pageData: null,
-            projectItems: null,
-            projectItemsStore: null
-        }
-    }
-    componentDidMount() {
-        db.database().ref().once('value').then(function(snapshot){
-            return snapshot.child('projects').val();
-        }).then((result) => {
-            this.setState({
-                pageData: result.page,
-                projectItems: result.projectItems,
-                projectItemsStore: result.projectItems
-            });
-        })
-    }
-    render() {
-        return (
-            <div className={this.state.pageData ? this.state.pageData.contentClass : ''}
-                 style={{backgroundImage: `url(${this.state.pageData ? this.state.pageData.background : ''})`}}>
-                <h2>{this.state.pageData ? this.state.pageData.title : ''}</h2>
-                <p className="tagline">{this.state.pageData ? this.state.pageData.tagline : ''}</p>
-                <div className="main"></div>
-                <div className="navicon" onClick={() => toggleMobile()}></div>
-                <Projectlist projectItems={this.state.projectItems ? this.state.projectItems : []}>
-                </Projectlist>
-            </div>
-        )
-    }
+function Projects() {
+  const [pageData, setPageData] = useState([]);
+  const [projectItems, setProjectItems] = useState([]);
+
+  useEffect(() => {
+    db.database()
+      .ref()
+      .once("value")
+      .then(function(snapshot) {
+        return snapshot.child("projects").val();
+      })
+      .then(result => {
+        setPageData(result.page);
+        setProjectItems(result.projectItems);
+      });
+  }, [...projectItems]);
+  return (
+    <div
+      className={pageData ? pageData.contentClass : ""}
+      style={{
+        backgroundImage: `url(${pageData ? pageData.background : ""})`
+      }}
+    >
+      <h2>{pageData ? pageData.title : ""}</h2>
+      <p className="tagline">{pageData ? pageData.tagline : ""}</p>
+      <div className="main"></div>
+      <div className="navicon" onClick={() => toggleMobile()}></div>
+      <Projectlist
+        projectItems={projectItems ? projectItems : []}
+      ></Projectlist>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
