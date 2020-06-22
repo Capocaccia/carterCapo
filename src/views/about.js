1499117b0a9ecbc2ad4ad86d28e2e4a79fdf96fa
Carter Capocaccia <carter.capocaccia@hilton.com>
2020-06-22 17:11:59 -0500

    re-writes about page as a functional component with hooks

--- src/views/about.js
+++ src/views/about.js
@@ -1,42 +1,41 @@
-import db from '../firebaseConfig'
-import React, { Component } from 'react';
-import Qaitems from '../components/qaItem'
+import db from "../firebaseConfig";
+import React, { useState, useEffect } from "react";
+import Qaitems from "../components/qaItem";
 import toggleMobile from "../mixins/toggleMobile";
 
-class About extends Component {
-    constructor() {
-        super()
-        this.state = {
-            pageData: null,
-            qa: null
-        }
-    }
-    componentDidMount() {
-        db.database().ref().once('value').then(function(snapshot){
-            return snapshot.child('about').val();
-        }).then((result) => {
-            this.setState({
-                pageData: result.page,
-                qa: result.aboutItems
-        });
-        })
-    }
-    render() {
-        return (
-            <div className={this.state.pageData ? this.state.pageData.contentClass : ''}
-                 style={{backgroundImage: `url(${this.state.pageData ? this.state.pageData.background : ''})`}}>
-                <h2>{this.state.pageData ? this.state.pageData.title : '' }</h2>
-                <p className="tagline">{this.state.pageData ? this.state.pageData.tagline : '' }</p>
-                <div className="main"></div>
-                <div className="navicon" onClick={() => toggleMobile()}></div>
-                <div className="qa">
-                    <Qaitems qas={this.state.qa ? this.state.qa : []}/>
-                </div>
-            </div>
-        )
-    }
+function About() {
+  const [pageData, setPageData] = useState([]);
+  const [aboutItems, setAboutItems] = useState([]);
+
+  useEffect(() => {
+    db.database()
+      .ref()
+      .once("value")
+      .then(function(snapshot) {
+        return snapshot.child("about").val();
+      })
+      .then(result => {
+        setPageData(result.page);
+        setAboutItems(result.aboutItems);
+      });
+  }, [...aboutItems]);
+
+  return (
+    <div
+      className={pageData ? pageData.contentClass : ""}
+      style={{
+        backgroundImage: `url(${pageData ? pageData.background : ""})`
+      }}
+    >
+      <h2>{pageData ? pageData.title : ""}</h2>
+      <p className="tagline">{pageData ? pageData.tagline : ""}</p>
+      <div className="main"></div>
+      <div className="navicon" onClick={() => toggleMobile()}></div>
+      <div className="qa">
+        <Qaitems qas={aboutItems ? aboutItems : []} />
+      </div>
+    </div>
+  );
 }
 
-export default About
-
-
+export default About;
