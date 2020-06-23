ad83cf7104ccab3da19d9d363cc46240f4067306
Carter Capocaccia <cartercapocaccia@Carters-MacBook-Pro.local>
2020-06-23 15:40:24 -0500

    Updates

---
+++ precache-manifest.cf24b64dbbe54bc4571603033b9f0964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcb95a9572a70c8db5cb751ca88fc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4face9b87f69e6248d3d",
+    "url": "/static/css/main.63da12da.chunk.css"
+  },
+  {
+    "revision": "3d3b849a2b52224acef4",
+    "url": "/static/js/2.77643fc9.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.77643fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4face9b87f69e6248d3d",
+    "url": "/static/js/main.4fb350e2.chunk.js"
+  },
+  {
+    "revision": "7afe935368f6db366c1f",
+    "url": "/static/js/runtime-main.6efd85b5.js"
+  }
+]);
