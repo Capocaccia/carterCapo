55ab7b7a1293b479abbbaa0f7d9bc14da5804932
Carter Capocaccia <carter.capocaccia@gmail.com>
2021-11-14 08:46:29 -0600

    adds analytics page view tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/index.css'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as ga from '../lib/ga'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
