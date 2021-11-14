39f46bed3c3a28e07d29f8eeb7ff5ae51b0d50c1
Carter Capocaccia <carter.capocaccia@gmail.com>
2021-11-14 08:46:50 -0600

    adds google analytics tracking script

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,25 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-112016254-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-112016254-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
