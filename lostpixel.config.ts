5790bd28e96a85787a85062bc3471e3fac385132
ccapocaccia <carter.capocaccia@hilton.com>
2022-09-21 11:01:50 -0500

    adds baseline images for lospixel and updates configuration for lostpixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,9 +2,14 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ id: "capocaccia.dev/", path: "/posts/uses", name: "Uses" }],
-    pageUrl: "http://localhost:3000/posts/uses",
+    pages: [
+      { id: "capocaccia.dev/posts/uses", path: "/posts/uses", name: "Uses" },
+      { id: "capocaccia.dev/", path: "/", name: "Home" },
+    ],
+    pageUrl: "http://localhost:3000",
   },
   generateOnly: true,
   failOnDifference: true,
+  waitBeforeScreenshot: 3000,
+  threshold: 0.3,
 };
