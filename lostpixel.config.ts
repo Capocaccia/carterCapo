571bfdafcb9901362a8d980e18351de534393517
ccapocaccia <carter.capocaccia@hilton.com>
2022-09-21 12:55:49 -0500

    removes old lostPixel.yml file and updates pageURL again to work agaisnt IP address from docker container in actions

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -6,7 +6,7 @@ export const config: CustomProjectConfig = {
       { id: "capocaccia.dev/posts/uses", path: "/posts/uses", name: "Uses" },
       { id: "capocaccia.dev/", path: "/", name: "Home" },
     ],
-    pageUrl: "http://127.0.0.1:3000",
+    pageUrl: "http://172.17.0.1:3000",
   },
   failOnDifference: true,
   generateOnly: true,
