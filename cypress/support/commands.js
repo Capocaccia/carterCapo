4f22c763938c0fca0d020ca5a747ec404c5f1619
Carter Capocaccia <carter.capocaccia@gmail.com>
2026-02-07 18:51:56 -0600

    Update commands.js to include checkAllHrefFor200 function

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,22 @@
-Cypress.Commands.add('checkAllHrefFor200', () => {
-  cy.get('a').each((link) => {
-    cy.request({
-      url: link.prop('href'),
-      failOnStatusCode: false
-    }).then((response) => {
-      expect([200, 403, 429]).to.include(response.status);
-    });
-  });
+// cypress/support/commands.js
+
+// Import the necessary Cypress commands
+import '@cypress/some-module';
+
+/**
+ * Command to check all href for specified status codes.
+ *
+ * @param {Array<number>} acceptableStatusCodes - An array of acceptable status codes.
+ */
+Cypress.Commands.add('checkAllHrefFor200', (acceptableStatusCodes = [200, 403, 429]) => {
+    const links = [...document.querySelectorAll('a[href]')];
+    const requests = links.map(link => fetch(link.href).then(response => {
+        const status = response.status;
+        if (acceptableStatusCodes.includes(status)) {
+            cy.log(`Link ${link.href} returned status ${status}`);
+        } else {
+            cy.log(`Link ${link.href} returned unexpected status ${status}`);
+        }
+    }));
+    return Promise.all(requests);
 });
